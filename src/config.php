f923bc514570772d77a55855f705db8852e41a15
SaurFort <tou.nathan@gmail.com>
2024-07-27 23:23:31 +0200

    Update config.php
    - Migrate error code 30 "PROJECT_ARGUMENT_ERROR" to 34 and rename it "PROJECT_READ_ARGUMENT_ERROR"
    - Add the error code 30 "PROJECT_ACTION_ERROR"

--- src/config.php
+++ src/config.php
@@ -1,19 +1,26 @@
 <?php
     // API Info
     define("NAME", "SFAPI");
-    define("VERSION", "0.4.0");
+    define("VERSION", "0.5.0");
     define("CREATOR", "SaurFort");
     define("AVAILABLE LANGUAGE", "en;fr");
 
     // API Key
     define("KEY_PREFIX", "apiv0_");
 
+    // API Permissions
+    define("PERMISSION_CREATE_PROJECTS", 0);
+    define("PERMISSION_UPDATE_PROJECTS", 1);
+    define("PERMISSION_DELETE_PROJECTS", 2);
+    define("PERMISSION_READ_PROJECTS", 3);
+
     // Error code
     define("EMPTY_API_KEY", "10");
     define("API_KEY_VERSION_ERROR", "11");
     define("API_KEY_PERMISSION_ERROR", "12");
     define("API_KEY_WRONG", "13");
-    define("PROJECT_ARGUMENT_ERROR", "30");
+    define("PROJECT_ACTION_ERROR", "30");
+    define("PROJECT_READ_ARGUMENT_ERROR", "34");
     define("SQL_QUERY_ERROR", "90");
     define("SQL_QUERY_EMPTY_ROW_ERROR", "91");
 ?>
