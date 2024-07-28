f706b1927b9b9bd3b96d2925a0cf41c3858145c4
SaurFort <97303132+SaurFort@users.noreply.github.com>
2024-07-28 11:11:22 +0200

    API V0.5.0 (#1)
    * Update config.php
    
    - Migrate error code 30 "PROJECT_ARGUMENT_ERROR" to 34 and rename it "PROJECT_READ_ARGUMENT_ERROR"
    - Add the error code 30 "PROJECT_ACTION_ERROR"
    
    * Update checker.php
    
    - Fix a major problem with permissions
    - Delete usefull data in query for API Key verification
    - Correction of spelling mistakes
    
    * Add create project possibility
    
    - Add two new error code: 31 "PROJECT_CREATE_ARGUMENT_ERROR" and 92 "SQL_PREPARE_ERROR"
    - Add the possibility to create project in the database
    - Migrate permissions check in a switch
    
    * Add update action in project
    
    The API is now able to update project and project's translations.
    
    * Add delete action in project
    
    Now the API is able to delete project and corresponding translations
    
    * Update en.md
    
    Only the English documentation have been updated because it's too long. x)

--- src/config.php
+++ src/config.php
@@ -1,19 +1,30 @@
 <?php
     // API Info
     define("NAME", "SFAPI");
-    define("VERSION", "0.4.0");
+    define("VERSION", "0.5.0");
     define("CREATOR", "SaurFort");
     define("AVAILABLE LANGUAGE", "en;fr");
 
     // API Key
     define("KEY_PREFIX", "apiv0_");
 
+    // API Permissions
+    define("PERMISSION_CREATE_PROJECTS", 0);
+    define("PERMISSION_UPDATE_PROJECTS", 1);
+    define("PERMISSION_DELETE_PROJECTS", 2);
+    define("PERMISSION_READ_PROJECTS", 3);
+
     // Error code
     define("EMPTY_API_KEY", "10");
     define("API_KEY_VERSION_ERROR", "11");
     define("API_KEY_PERMISSION_ERROR", "12");
     define("API_KEY_WRONG", "13");
-    define("PROJECT_ARGUMENT_ERROR", "30");
+    define("PROJECT_ACTION_ERROR", "30");
+    define("PROJECT_CREATE_ARGUMENT_ERROR", "31");
+    define("PROJECT_UPDATE_ARGUMENT_ERROR", "32");
+    define("PROJECT_DELETE_ARGUMENT_ERROR", "33");
+    define("PROJECT_READ_ARGUMENT_ERROR", "34");
     define("SQL_QUERY_ERROR", "90");
     define("SQL_QUERY_EMPTY_ROW_ERROR", "91");
+    define("SQL_PREPARE_ERROR", "92");
 ?>
