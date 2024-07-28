a8af5bf33844e761173592d71541bd708b0d52ab
SaurFort <tou.nathan@gmail.com>
2024-07-28 15:32:41 +0200

    Update comments

--- src/api/checker.php
+++ src/api/checker.php
@@ -3,7 +3,7 @@
         if (empty($key)) {
             echo json_encode(["code" => EMPTY_API_KEY, "message" => "You haven't set your API key."]);
             exit; // Add an exit here to ensure execution stops.
-        } elseif (str_starts_with($key, KEY_PREFIX)) { // Version verification
+        } elseif (str_starts_with($key, KEY_PREFIX)) { // Check version
             include('../database.php');
 
             $query = "SELECT api.api_key, api.perms FROM api_keys api WHERE api.api_key = ?";
@@ -39,7 +39,7 @@
             // Remove any trailing spaces and semicolons
             $keyPerms = trim($keyPerms, " ;");
             
-            // Perms structure is "CREATE_PROJECTS;UPDATE_PROJECTS;DELETE_PROJECTS;READ_PROJECTS"
+            // Perms structure is in the file config.php
             $perms = explode(";", $keyPerms);
 
             // Debug: Display permissions after splitting into array
