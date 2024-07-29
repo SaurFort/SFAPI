5142e3459a0b56a2f81aaab1c6f7e547c2fafae2
SaurFort <97303132+SaurFort@users.noreply.github.com>
2024-07-29 10:40:47 +0200

    Add logs to register API
    - Register API is now logged
    - Logger was no longer taking IP
    - Gitignore will no longer add logs folder to GitHub

--- src/api/account/register.php
+++ src/api/account/register.php
@@ -5,6 +5,7 @@
     require('../../config.php');
     include('../database.php');
     include('../checker.php');
+    include('../logger.php');
 
     $key = isset($_GET['key']) ? $_GET['key'] : "";
 
@@ -23,18 +24,22 @@
 
         // Validate required fields
         if (empty($data['username'])) {
+            makeLog("API-Register", $key, "No username provided", 2);
             echo json_encode(["code" => ACCOUNT_REGISTER_ARGUMENT_ERROR . "A", "message" => "Missing or undefined value for username."]);
             exit;
         }
         if (empty($data['email'])) {
+            makeLog("API-Register", $key, "No email address provided", 2);
             echo json_encode(["code" => ACCOUNT_REGISTER_ARGUMENT_ERROR . "B", "message" => "Missing or undefined value for email."]);
             exit;
         }
         if (empty($data['password'])) {
+            makeLog("API-Register", $key, "No password provided", 2);
             echo json_encode(["code" => ACCOUNT_REGISTER_ARGUMENT_ERROR . "C", "message" => "Missing or undefined value for password."]);
             exit;
         }
         if (empty($data['confirmationPassword'])) {
+            makeLog("API-Register", $key, "No password confirmation provided", 2);
             echo json_encode(["code" => ACCOUNT_REGISTER_ARGUMENT_ERROR . "D", "message" => "Missing or undefined value for confirmationPassword."]);
             exit;
         }
@@ -54,6 +59,7 @@
         
         // Checks whether the password has already been hashed
         if (isAlreadyHashed($data['password'], $hashPrefixes)) {
+            makeLog("API-Register", $key, "Password alreay hashed but hash is not supported", 2);
             echo json_encode(["code" => ACCOUNT_REGISTER_PASSWORD_ERROR . "A", "message" => "Password is already hashed and the algorithm used is not supported by the API."]);
             exit;
         }
@@ -68,10 +74,12 @@
         if (str_starts_with($password, "\$argon2id\$v=19\$m=65536,t=4,p=1")) {
             $hashedPassword = $password;
         } elseif (str_starts_with($password, "\$argon2id")) {
+            makeLog("API-Register", $key, "Password hashed but not with the valid parameters.", 2);
             echo json_encode(["code" => ACCOUNT_REGISTER_PASSWORD_ERROR . "B", "message" => "Password hash not used the correct parameter."]);
             exit;
         } else {
             if ($password !== $passwordConfirmation) {
+                makeLog("API-Register", $key, "Password and password confirmation are not the same", 2);
                 echo json_encode(["code" => ACCOUNT_REGISTER_PASSWORD_ERROR . "C", "message" => "Password and password confirmation are not the same."]);
                 exit;
             }
@@ -81,6 +89,7 @@
 
         // Verify if password is correctly hashed
         if (!password_verify($passwordConfirmation, $hashedPassword)) {
+            makeLog("API-Register", $key, "The password was not hashed correctly", 3);
             echo json_encode(["code" => ACCOUNT_REGISTER_PASSWORD_ERROR . "D", "message" => "The password was not hashed correctly."]);
             exit;
         }
@@ -90,6 +99,7 @@
         $stmt = $conn->prepare($checkQuery);
 
         if ($stmt === false) {
+            makeLog("API-Register", $key, "SQL Error: " . $conn->error, 2);
             echo json_encode(["code" => SQL_PREPARE_ERROR, "message" => "SQL Error: " . $conn->error]);
             exit;
         }
@@ -99,6 +109,7 @@
         $stmt->store_result();
 
         if ($stmt->num_rows > 0) {
+            makeLog("API-Register", $key, "Username: $username or Email: $email are already registered", 3);
             echo json_encode(["code" => ACCOUNT_REGISTER_DUPLICATE_ERROR, "message" => "Username or email already exists."]);
             $stmt->close();
             exit;
@@ -109,16 +120,20 @@
         $query = "INSERT INTO login (username, email, password, `rank`) VALUES (?, ?, ?, ?)";
         $stmt = $conn->prepare($query);
         if ($stmt === false) {
+            makeLog("API-Register", $key, "SQL Prepare Error: " . $conn->error, 2);
             echo json_encode(["code" => SQL_PREPARE_ERROR, "message" => "SQL Error: " . $conn->error]);
             exit;
         }
         $stmt->bind_param("ssss", $username, $email, $hashedPassword, $rank);
         if ($stmt->execute()) {
+            makeLog("API-Register", $key, "Account of $username was registered successfully.", 3);
             echo json_encode(["code" => QUERY_WORKED_SUCCESSFULLY, "message" => "User registered successfully."]);
         } else {
+            makeLog("API-Register", $key, "SQL Error: " . $conn->error, 2);
             echo json_encode(["code" => SQL_QUERY_ERROR, "message" => "SQL Error: " . $stmt->error]);
         }
         $stmt->close();
+        exit;
     } else {
         echo json_encode(["code" => INVALID_API_METHOD, "message" => "Register can only take POST method"]);
         exit;
--- src/api/logger.php
+++ src/api/logger.php
@@ -4,14 +4,14 @@
 
         $date = date("Y-m-d"); // Format the date for the log file name
         $timestamp = date("H:i:s"); // Get the current time for the log entry
-        $userIP = getUserIP(); // Get the IP address of the user
+        //$userIP = getUserIP(); // Get the IP address of the user
         $fileName = LOGS_FOLDER . $date . ".log"; // Create the log file name using the current date
 
         if (!file_exists(LOGS_FOLDER)) {
             mkdir(LOGS_FOLDER, 0777, true);
         }
 
-        $logMessage = "$severity: [$timestamp] [$apiRequested] | [$userIP]: " . $message . " - Key: " . $apiKey . "\n"; // Create the log message with a newline
+        $logMessage = "$severity: [$timestamp] [$apiRequested]: " . $message . " - Key: " . $apiKey . "\n"; // Create the log message with a newline
 
         // Debugging output
         // echo($fileName);
@@ -20,7 +20,7 @@
         file_put_contents($fileName, $logMessage, FILE_APPEND);
     }
 
-    function getUserIP() {
+    /*function getUserIP() {
         // Check for shared internet/ISP IPs
         if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
             $ip = $_SERVER['HTTP_CLIENT_IP'];
@@ -34,5 +34,5 @@
             $ip = $_SERVER['REMOTE_ADDR'];
         }
         return $ip;
-    }
+    }*/
 ?>
