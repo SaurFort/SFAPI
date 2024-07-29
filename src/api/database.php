3f6b0096b3ae0c76010bc7039badfe2836481847
SaurFort <97303132+SaurFort@users.noreply.github.com>
2024-07-29 16:13:20 +0200

    Update database credentials
    Now the database credentials are stored in config.php

--- src/api/database.php
+++ src/api/database.php
@@ -1,10 +1,5 @@
 <?php
-    $servername = "localhost:3306";
-    $username = "root";
-    $password = "";
-    $dbname = "saurfort";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
--- src/config.php
+++ src/config.php
@@ -1,15 +1,28 @@
 <?php
     // API Info
     define("NAME", "SFAPI");
-    define("VERSION", "0.6.0");
+    define("VERSION", "0.7.0");
     define("CREATORS", "SaurFort");
-    define("AVAILABLE LANGUAGE", "en;fr");
+    define("AVAILABLE_LANGUAGES", "en;fr");
 
     // API settings
-    define("LOGS_FOLDER", "../../logs/"); // Path to logs folder
+    define("LOGS_FOLDER", "../../logs/");   // Path to logs folder
+    define("KEY_PREFIX", "apiv0_");         // API Key prefix
 
-    // API Key
-    define("KEY_PREFIX", "apiv0_");
+    // Database 
+    define("DB_SERVER", "localhost:3306");
+    define("DB_NAME", "saurfort");
+    define("DB_USERNAME", "root");
+    define("DB_PASSWORD", "");
+
+    // Mailing API (SMTP)
+    define("MAILING_ENABLED", false);                   // If you can access to a mailing services so you can enabled it
+    define("SMTP_SERVER" , "");                         // Put your out server using SMTP
+    define("SMTP_PORT" , "");                           // default: 465
+    define("SMTP_AUTH" , true);                         // If your SMTP server request a password set it on true, else set it on false
+    define("EMAIL_ADDRESS" , "");                       // Recommended: use a noreply email address.
+    define("EMAIL_NAME", "");                           // The name you want to be showed when user received an email of the API
+    define("EMAIL_PASSWORD" , "");                      // If auth is set to true you need to fill this
 
     // API Permissions
     define("PERMISSION_REGISTER_USER", 0);
@@ -18,8 +31,9 @@
     define("PERMISSION_READ_PROJECTS", 3);
     define("PERMISSION_UPDATE_PROJECTS", 4);
     define("PERMISSION_DELETE_PROJECTS", 5);
+    define("PERMISSION_SEND_MAIL", 6);
 
-    // Error code
+    // API Code
     define("QUERY_WORKED_SUCCESSFULLY", "01");
     define("INVALID_API_METHOD", "02");
     define("EMPTY_API_KEY", "10");
@@ -31,6 +45,9 @@
     define("ACCOUNT_REGISTER_DUPLICATE_ERROR", "16");
     define("ACCOUNT_LOGIN_ARGUMENT_ERROR", "17");
     define("ACCOUNT_LOGIN_FAILED", "18");
+    define("MAILER_DISABLED", "20");
+    define("MAILER_SEND_EMAIL_ARGUMENT_ERROR", "21");
+    define("MAILER_SEND_EMAIL_FAILED", "22");
     define("PROJECT_ACTION_ERROR", "30");
     define("PROJECT_CREATE_ARGUMENT_ERROR", "31");
     define("PROJECT_UPDATE_ARGUMENT_ERROR", "32");
